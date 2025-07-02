f792b833cf3436f8d4f4f4cac8cc79282d8bd725
Jimmy FMT <jimmy@Jimmys-MacBook-Pro.local>
2025-07-02 22:13:50 +0800

    Resolved middleware checking pending approval

--- routes/admin.php
+++ routes/admin.php
@@ -67,7 +67,7 @@ Route::middleware(['auth:admin'])->scopeBindings()->group(function () {
             Route::get('{booking}/detail', [DraftBookingController::class, 'detail'])->name('.detail');
         });
 
-        Route::prefix('pending-approvals')->name('.pending-approval')->middleware('admin.role:super_admin')->group(function () {
+        Route::prefix('pending-approvals')->name('.pending-approval')->group(function () {
             Route::get('', [PendingApprovalController::class, 'index']);
             Route::post('query', [PendingApprovalController::class, 'pendingReviewQuery'])->name('.query');
             // Route::get('create', [PendingApprovalController::class, 'create'])->name('.create');
